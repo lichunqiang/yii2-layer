2cf980d1083969b9025cfa1f32a127f62be359a3
lichunqiang <light-li@hotmail.com>
2015-04-24 23:52:46 +0800

    v0.1.1

--- LayerAsset.php
+++ LayerAsset.php
@@ -1,7 +1,15 @@
 <?php namespace light\assets;
 
 use yii\web\AssetBundle;
-
+/**
+ * ~~~
+ * use light\assets\LayerAsset;
+ *
+ * LayerAsset::register($this);
+ * ~~~
+ * @version 0.1.1
+ * @author light-li<light-li@hotmail.com>
+ */
 class LayerAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/light/yii2-layer/source/layer/1.9.0';
---
+++ LayerMobileAsset.php
@@ -0,0 +1,20 @@
+<?php namespace ligh\assets;
+
+use yii\web\AssetBundle;
+/**
+ * ~~~
+ * use light\assets\LayerMobileAsset;
+ *
+ * LayerMobielAsset::register($this);
+ * ~~~
+ * @version 0.1.1
+ * @author light-li<light-li@hotmail.com>
+ */
+class LayerMobileAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/light/yii2-layer/source/layer/1.9.0';
+
+    public $js = ['layer.m.js'];
+
+    public $css = ['need/layer.css'];
+}
